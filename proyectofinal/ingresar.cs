e68756012e3da5678188198dfa326e1edde88be4
Margarito Hernandez <Margarito Hernandez@MARGARITO>
2018-06-10 13:23:59 -0600

    inentario a lamitad

---
+++ proyectofinal/ingresar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyectofinal
+{
+    class ingresar
+    {
+        string nomproduc;
+        string categoria;
+        string precio;
+        string cantidad;
+
+        public string Nomproduc { get => nomproduc; set => nomproduc = value; }
+        public string Categoria { get => categoria; set => categoria = value; }
+        public string Precio { get => precio; set => precio = value; }
+        public string Cantidad { get => cantidad; set => cantidad = value; }
+    }
+}

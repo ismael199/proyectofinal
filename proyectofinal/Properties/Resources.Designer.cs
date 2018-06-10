b66f7103285192a2d6faf3e8f861eab4570efb28
Margarito Hernandez <Margarito Hernandez@MARGARITO>
2018-06-10 11:59:06 -0600

    administradoy modificacion y inventario de administrador terminado creo jajaj

--- proyectofinal/Properties/Resources.Designer.cs
+++ proyectofinal/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace proyectofinal.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap oooo {
+            get {
+                object obj = ResourceManager.GetObject("oooo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
